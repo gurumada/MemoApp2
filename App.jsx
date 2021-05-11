0b8131562c7742c5f02d6d81263b3de2efec819e
SUGURU <the.rain.let.up@gmail.com>
2021-05-11 22:59:42 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import MemolistScreen from "./src/screens/MemoListScreen";
 import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+import MemoEditScreen from "./src/screens/MemoEditScreen";
+import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 
 export default function App() {
   return (
-    <MemolistScreen />
+    <MemoCreateScreen />
   );
 }
